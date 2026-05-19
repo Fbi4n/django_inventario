050580a4518bac9bae69ab2390962b391ad42c91
Fabian <contrerafabian714@gmail.com>
2026-05-19 13:35:00 -0300

    100% Completado

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,13 +78,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'inventario_4p5a'),
-        'USER': os.getenv('DB_USER', 'inventario_4p5a_user'),
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': os.getenv('DB_NAME'),
+
+        'USER': os.getenv('DB_USER'),
+
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+
+        'HOST': os.getenv('DB_HOST'),
+
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
